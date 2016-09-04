159e2b620753b2c062fb1d784124b1efbb49a1ff
RongleXie <252254002@qq.com>
2016-09-04 18:02:28 +0800

    update_third
    CarData

---
+++ src/com/yesbulo/cardatacolsystem/action/CardataAction.java
@@ -0,0 +1,12 @@
+package com.yesbulo.cardatacolsystem.action;
+
+/**
+ * <p>@Title:CardataAction</P>
+ * <p>@Description:carDataColSystem</P>
+ * <p>@Company: RongleXie </P>
+ * <p>@author xieqingrong</p>
+ * <p>@date 2016-9-4 下午01:30:18
+ */
+public class CardataAction {
+	
+}
